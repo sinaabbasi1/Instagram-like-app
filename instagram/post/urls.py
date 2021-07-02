379fd4ec56f71c4e033b7808bb0133fcd29ebb31
OmidAmirkhani <omid538@gmail.com>
2021-07-02 18:40:46 +0430

    updated urls.py --tags

--- instagram/post/urls.py
+++ instagram/post/urls.py
@@ -1,9 +1,10 @@
-from post.views import index, PostDetails, NewPost
+from post.views import index, PostDetails, NewPost, tags
 
 from django.urls import path
 
 urlpatterns = [
    	path('', index, name='index'),
    	path('<uuid:post_id>', PostDetails, name='postdetails'),
-	  path('newpost/', NewPost, name='newpost'),
+	path('newpost/', NewPost, name='newpost'),
+   	path('tag/<slug:tag_slug>', tags, name='tags'),
 ]

83309d63c720ecbe98ac3c9c5c24cf23a59dc7f2
OmidAmirkhani <86747745+OmidAmirkhani@users.noreply.github.com>
2021-07-03 12:29:17 +0430

    update urls.py --ShowNOtifications-DeleteNotification

--- instagram/notifications/urls.py
+++ instagram/notifications/urls.py
@@ -4,5 +4,6 @@ from notifications.views import ShowNOtifications, DeleteNotification
 
 urlpatterns = [
    	path('', ShowNOtifications, name='show-notifications'),
+   	path('<noti_id>/delete', DeleteNotification, name='delete-notification'),
 
 ]
